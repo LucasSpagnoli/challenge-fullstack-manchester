ec56fe820294286226927430a04eeb7307aa3ba0
Lucas Spagnoli <lm.spanholi@gmail.com>
2026-06-11 09:15:05 -0300

    fix: fix getFeed auth guard

--- backend/src/feed/feed.controller.ts
+++ backend/src/feed/feed.controller.ts
@@ -4,28 +4,30 @@ import { JwtAuthGuard } from 'src/auth/Guards/jwt.guard';
 import { FeedService } from './feed.service';
 
 @Controller('feed')
-// @UseGuards(JwtAuthGuard)
+@UseGuards(JwtAuthGuard)
 export class FeedController {
 
     constructor(
         private feedService: FeedService,
     ) { }
 
-    @Get('/infomoney')
-    async getNews() {
-        return this.feedService.getParsedNews()
-    }
-
-    @Get(':id')
-    async getFeed(@Param('id', ParseIntPipe) userId: number) {
-        // async getFeed(@Req() req: Request) {
-        // const userId = req.user?.id
+    @Get()
+    async getFeed(@Req() req: Request) {
+        if (!req.user) {
+            throw new BadRequestException("Usuário ausente")
+        }
+        const userId = req.user.id
         if (!userId) {
             throw new BadRequestException("ID de usuário ausente")
         }
         const news = await this.feedService.getParsedNews()
         const preferences = await this.feedService.getPreferences(userId)
         return this.feedService.geminiCall({ news, preferences })
+    }
+
+    @Get('/news')
+    async getNews() {
+        return this.feedService.getParsedNews()
     }
 
     @Post()
