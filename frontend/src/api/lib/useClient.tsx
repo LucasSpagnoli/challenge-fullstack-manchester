86f2345d91e8b871fa153f76d623dd08e062cc8d
Lucas Spagnoli <lm.spanholi@gmail.com>
2026-08-06 18:34:57 -0300

    fix: fix update refresh page

--- frontend/src/api/lib/useClient.tsx
+++ frontend/src/api/lib/useClient.tsx
@@ -40,7 +40,17 @@ export function useClient() {
         setError(null)
         try {
             const updatedClient = await apiUpdateClient(client_id, payload)
-            setClients((prev) => prev.map((c) => (c.client_id === client_id ? updatedClient : c)));
+
+            setClients((prev) => prev.map((c) => {
+                if (c.client_id === client_id) {
+                    return {
+                        ...c,
+                        ...updatedClient,
+                        client_id: client_id
+                    }}
+
+                return c
+            }))
             return updatedClient
         } catch (err) {
             setError(err instanceof Error ? err.message : "Erro ao atualizar cliente.");
